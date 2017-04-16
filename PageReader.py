894c86f05dc8a3f7b1afe1e3932bd6372ea44c70
Glen MacKay <glen@mackaytech.com>
2017-04-16 15:48:22 -0700

    Matching PEP8 style guide for function/var names

--- PageReader.py
+++ PageReader.py
@@ -14,7 +14,7 @@ class PageReader:
         'targets': {
             'company': {
                 'xpath': '//*[@id="category-2"]/article/ul/li[*]/a/span[1]',
-                'type': 'fullText'
+                'type': 'full_text'
             },
             'title': {
                 'xpath': '//*[@id="category-2"]/article/ul/li[*]/a/span[2]',
@@ -39,93 +39,81 @@ class PageReader:
         }
     }
 
-    def getPage(self) :
+    def get_page(self) :
         url = (self.config['schema'] + '://' + self.config['host']
             + '/' + self.config['path'])
-        pageRequest = requests.get(url)
-        return pageRequest.content
+        page_request = requests.get(url)
+        return page_request.content
 
-    def getXPathResults(self, pageContent):
-        tree = html.fromstring(pageContent);
+    def get_xpath_results(self, page_content):
+        tree = html.fromstring(page_content);
         results = {}
-        pathsDict = self.config['xpath']
-        for key, xpathString in pathsDict.items():
+        for key, target_data in self.config['targets'].items():
             if not key in results:
                 results[key] = []
-            xpathResults = tree.xpath(xpathString + '/text()')
-            results[key] = results[key] + xpathResults
+            xpath_results = tree.xpath(target_data['xpath'] + '/text()')
+            results[key] = results[key] + xpath_results
         return results
 
-    def getXPathResults(self, pageContent):
-        tree = html.fromstring(pageContent);
-        results = {}
-        for key, targetData in self.config['targets'].items():
-            if not key in results:
-                results[key] = []
-            xpathResults = tree.xpath(targetData['xpath'] + '/text()')
-            results[key] = results[key] + xpathResults
-        return results
-
-    def analyzeResults(self, results):
+    def analyze_results(self, results):
         for key, data in results.items():
-            func = self.getAnalyzeStrategy(key)
+            func = self.get_analyze_strategy(key)
             try:
                 getattr(self, func)(data, key)
             except AttributeError:
                 print('Expected function ' + func + ' does not exist')
 
-    def analyzeFullText(self, data, key):
+    def analyze_full_text(self, data, key):
         counts = {}
         for text in data:
-            counts = self.addPhraseCount(counts, text)
-        countsSorted = sorted(
+            counts = self.add_phrase_count(counts, text)
+        counts_sorted = sorted(
             counts.items(),
             key=operator.itemgetter(1),
             reverse=True
         )
-        for text, count in countsSorted:
+        for text, count in counts_sorted:
             print(str(count) + ' ' + text)
 
-    def analyzeWords(self, data, key):
+    def analyze_words(self, data, key):
         counts = {};
-        useWhitelist = self.hasWhitelist(key)
-        if (useWhitelist):
+        use_whitelist = self.has_whitelist(key)
+        if (use_whitelist):
             whitelist = self.config['targets'][key]['whitelist']
         for text in data:
             words = text.split()
             for word in words:
                 word = re.sub(r"[^a-z\+#]", "", word.lower())
-                if (useWhitelist):
+                if (use_whitelist):
                     try:
                         whitelist.index(word)
-                        counts = self.addPhraseCount(counts, word)
+                        counts = self.add_phrase_count(counts, word)
                     except ValueError:
                         pass
                 else:
-                    counts = self.addPhraseCount(counts, word)
-        countsSorted = sorted(
+                    counts = self.add_phrase_count(counts, word)
+        counts_sorted = sorted(
             counts.items(),
             key=operator.itemgetter(1),
             reverse=True
         )
-        for target, count in countsSorted:
+        for target, count in counts_sorted:
             print(str(count) + ' ' + target)
 
-    def addPhraseCount(self, counts, word):
+    def add_phrase_count(self, counts, word):
         if not word in counts:
             counts[word] = 0
         counts[word] += 1
         return counts
 
-    def hasWhitelist(self, key):
+    def has_whitelist(self, key):
         return not self.config['targets'][key]['whitelist'] is None
 
-    def getAnalyzeStrategy(self, targetKey):
-        analyzeType = self.config['targets'][targetKey]['type']
-        analyzeType = analyzeType[0].upper() + analyzeType[1:]
-        return 'analyze' + analyzeType
+    def get_analyze_strategy(self, target_key):
+        analyze_type = self.config['targets'][target_key]['type']
+        return 'analyze_' + analyze_type
 
-pageReader = PageReader()
-content = pageReader.getPage()
-results = pageReader.getXPathResults(content)
-analyzed = pageReader.analyzeResults(results)
+page_reader = PageReader()
+content = page_reader.get_page()
+results = page_reader.get_xpath_results(content)
+analyzed = page_reader.analyze_results(results)
