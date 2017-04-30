4ecb86c460832b392a3de2db46bb71bbb08f3f13
Glen MacKay <glen@mackaytech.com>
2017-04-30 14:48:26 -0700

    Added functionality to analyze scrape data and ouput the results to a csv file

--- PageReader.py
+++ PageReader.py
@@ -2,23 +2,26 @@ from lxml import html
 import requests
 import re
 
+# Reads individual web pages and analyzes them based on config info
 class PageReader:
-
     # page and xpath info, see config.json
     config = None
 
+    # Main public function to analyze a page based on config
     def analyze_page():
         page_reader = PageReader()
         content = page_reader._get_page()
         results = page_reader._get_xpath_results(content)
         return page_reader._analyze_results(results)
 
+    # Fetches page content via an HTTP request
     def _get_page(self) :
         url = (self.config['schema'] + '://' + self.config['host']
             + '/' + self.config['path'])
         page_request = requests.get(url)
         return page_request.content
 
+    # Runs an xpath query on page content and returns the results
     def _get_xpath_results(self, page_content):
         tree = html.fromstring(page_content);
         results = {}
@@ -29,6 +32,8 @@ class PageReader:
             results[key] = results[key] + xpath_results
         return results
 
+    # Strategy pattern function that calls the analyze strategy found in
+    # the config. Prints an error if the function cannot be found
     def _analyze_results(self, results):
         analyzed = {};
         for key, data in results.items():
@@ -39,18 +44,22 @@ class PageReader:
                 print('Expected function ' + func + ' does not exist')
         return analyzed
 
+    # Gets instances of the full text value (lowercased)
     def _analyze_full_text(self, data, key):
         counts = {}
         for text in data:
             counts = self._add_phrase_count(counts, text.lower())
         return counts
 
+    # Gets instances of a link
     def _analyze_link(self, data, key):
         counts = {}
         for text in data:
             counts = self._add_phrase_count(counts, text)
         return counts
 
+    # Gets instances of individual words (lowercased), optionally
+    # using a whitelist (only returns words found the the whitelist)
     def _analyze_words(self, data, key):
         counts = {};
         use_whitelist = self._has_whitelist(key)
@@ -70,15 +79,19 @@ class PageReader:
                     counts = self._add_phrase_count(counts, word)
         return counts
 
-    def _add_phrase_count(self, counts, word):
-        if not word in counts:
-            counts[word] = 0
-        counts[word] += 1
+    # Helper function to increment the count of a phrase, adding the
+    # key to the counts dict if it doesn't exist
+    def _add_phrase_count(self, counts, phrase):
+        if not phrase in counts:
+            counts[phrase] = 0
+        counts[phrase] += 1
         return counts
 
+    # Helper function to determine if a target uses a whitelist
     def _has_whitelist(self, key):
         return not self.config['targets'][key]['whitelist'] is None
 
+    # Helper function to get the function name of the analyze strategy
     def _get_analyze_strategy(self, target_key):
         analyze_type = self.config['targets'][target_key]['type']
         return '_analyze_' + analyze_type
--- SiteScraper.py
+++ SiteScraper.py
@@ -1,62 +1,106 @@
 import json
-from collections import OrderedDict
-from lxml import html
-import requests
+import csv
+import time
 from PageReader import PageReader
 
+# Main class, loads config, runs the scrape, and handles scrape output
 class SiteScraper:
+    # Config data loaded from JSON
     config = None
+    # Raw page scraper results
     results = {}
+    # Results after analysis (collation, key counting, etc.)
+    analyzed_results = {}
 
+    # Loads config from a JSON file
     def load_config(self, config_path):
         json_data = open(config_path).read()
         data = json.loads(json_data)
         self.config = data
 
+    # Main run function, scrapes all pages in the config and outputs results
     def run_scrape(self):
         for key, page in self.config['pages'].items():
-            print(key)
-            # print(page)
-            self.results[key] = self.get_page_reader_results(page)
-            # print(self.results)
-            print("\n")
-        self.analyze_scrape()
+            self.results[key] = self._get_page_reader_results(page, True)
+        self._analyze_scrape()
+        self._output_results()
 
-    def analyze_scrape(self):
-        targets = {}
-        for post_results in self.results['posts']:
-            keys = post_results['body'].keys()
-            for key in keys:
-                if not key in targets:
-                    targets[key] = 0
-                targets[key] += 1
-        targets = OrderedDict(sorted(targets.items(), key=lambda x:x[1]))
-        print(targets)
+    # Runs through results and counts key occurances if config has the
+    # xpath target set to save the results
+    def _analyze_scrape(self):
+        target_results = {}
+        for key, results in self.results.items():
+            for result in results:
+                for target, data in result.items():
+                    if self._should_save_results(key, target) == False:
+                        continue;
+                    if not target in target_results:
+                        target_results[target] = {}
+                    target_keys = data.keys()
+                    for target_key in target_keys:
+                        if not target_key in target_results.get(target, {}):
+                            target_results[target][target_key] = 0
+                        target_results[target][target_key] += 1
+        self.analyzed_results = target_results
 
+    # Determines output strategy from config and calls the function
+    # prints an error if the function doesn't exist
+    def _output_results(self):
+        output_strategy = '_output_' + self.config['output']
+        try:
+            getattr(self, output_strategy)()
+        except AttributeError:
+            print('Expected function ' + output_strategy + ' does not exist')
 
-    def get_page_reader_results(self, page):
+    # Saves analyzed results by appending them to a csv file
+    def _output_csv(self):
+        print(self.analyzed_results)
+        date = time.strftime("%m-%d-%Y %H:%M:%S")
+        for target, data in self.analyzed_results.items():
+            filename = self.config['host'] + '.' + target + '.csv'
+            with open(filename, 'a', newline='') as file_handle:
+                writer = csv.writer(file_handle, delimiter=',')
+                for key, value in data.items():
+                    writer.writerow([date, key, value])
+
+    # Helper function to determine if the target results should be 
+    # included in the output
+    def _should_save_results(self, key, target):
+        target_config = self.config['pages'][key]['targets'][target]
+        if not 'save' in target_config:
+            return False
+        if (target_config['save'] == False):
+            return False
+        return True
+
+    # Calls into PageReader to scrape the site by running through each
+    # page from the config, referencing urls from a previous page if specified
+    def _get_page_reader_results(self, page, top_level = False):
         PageReader.config = page
         if (page['path']['type'] == 'url'):
             PageReader.config['path'] = page['path']['value']
         else:
             reference_page = page
             page_keys = page['path']['value'].split('.')
-            path_values = self.results[page_keys[0]][page_keys[1]]
+            path_values = self.results[page_keys[0]][0][page_keys[1]]
             results = []
             for path in path_values:
                 reference_page['path'] = {
                     'value': str(path),
                     'type': 'url',
                 }
-                result = self.get_page_reader_results(reference_page);
+                result = self._get_page_reader_results(reference_page);
                 print(result)
                 results.append(result)
             return results
         PageReader.config['schema'] = self.config['schema']
         PageReader.config['host'] = self.config['host']
-        return PageReader.analyze_page()
+        final_results = PageReader.analyze_page()
+        if top_level == True:
+            final_results = [final_results]
+        return final_results
 
+# Load up the config and run the site scrape!
 site_scaper = SiteScraper()
 site_scaper.load_config('./config.json')
 results = site_scaper.run_scrape()
-# print(results)
